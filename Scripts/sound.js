5fe08a244050b502b08bf4ed139e6262e5df1bf9
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2014-11-16 15:19:03 +0200

    changes

--- Scripts/sound.js
+++ Scripts/sound.js
@@ -1,7 +1,7 @@
 /**
  * Created by Stoilkov on 16.11.2014 г..
  */
-function balloonPoppingSound() {
+function BalloonPoppingSound() {
     this.sound = document.getElementById("balloonPopping");
     this.play = function play() {
         this.sound.play();
